c562a5feaddaee1bf294ca490903c0a295c16fae
Sean Lucius <seanlucius22@icloud.com>
2018-11-04 20:26:47 +0000

    automatically backed up by learn

--- challenge.rb
+++ challenge.rb
@@ -16,5 +16,5 @@ end
   
   
   
-name_arr = ["Edsger", "Ada", "Charles", "Alan", ""Grace", Linus, Matz]
-puts batch_badge_creator(name_arr)
+name_arr = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+puts batch_badge(name_arr)
